f27789d18e0e1a720f703d46ac9a25561703ee97
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-01-15 21:01:26 -0500

    Update FileMenuHandler.java

--- FileMenuHandler.java
+++ FileMenuHandler.java
@@ -9,7 +9,7 @@ import java.util.TreeMap;
 
 
 public class FileMenuHandler implements ActionListener{
-	  public static int tokens;								//needed variables and instances of objects
+	  public static int tokens;					        //needed variables and instances of objects
 	  public static TextFileInput DateFile,DateFile2;
 	  public static StringTokenizer DateTokens,DateTokens2;
 	  public static Date212[] DateArray;
@@ -21,9 +21,9 @@ public class FileMenuHandler implements ActionListener{
 	   date = jf;
 	 }
 	  
-  public void actionPerformed(ActionEvent event) {		//method which how the
-	      String menuName = event.getActionCommand();		//file menu reacts when 
-	      if (menuName.equals("Open"))						//different buttons are clicked on
+  public void actionPerformed(ActionEvent event) {		//method which tells the program how to
+	      String menuName = event.getActionCommand();	//react when 
+	      if (menuName.equals("Open"))			//different buttons are clicked on
 	            openFile(); 
 	      else if (menuName.equals("Quit"))
 	         System.exit(0);
@@ -36,30 +36,30 @@ public class FileMenuHandler implements ActionListener{
 		 DateFile = new TextFileInput(file1);
 		  line = DateFile.readLine();
 
-	     while (line!=null) {									// count the amount of tokens
+	     while (line!=null) {					// count the amount of tokens
 		    DateTokens = new StringTokenizer(line,",");
 		 	tokens += DateTokens.countTokens();
 		 		line = DateFile.readLine();
 		 				}
-		 	dateListUnsorted = new String[tokens];				// declare an array with the size of the tokens
+		 	dateListUnsorted = new String[tokens];		// declare an array with the size of the tokens
 		 	DateFile2 = new TextFileInput(file1);
 			 line2 = DateFile2.readLine();
 			 			
-	          while (line2!= null) {								//while there are more lines, create tokens
+	          while (line2!= null) {				//while there are more lines, create tokens
 		      DateTokens2 = new StringTokenizer(line2,",");	
-		   while (DateTokens2.hasMoreTokens()) {				//while there are more tokens, append the tokens to the array
+		   while (DateTokens2.hasMoreTokens()) {		//while there are more tokens, append the tokens to the array
 		 	dateListUnsorted[k++] = DateTokens2.nextToken();		
 		 			}
-		 	    line2= DateFile2.readLine();				//read the next line
+		 	    line2= DateFile2.readLine();		//read the next line
 		 				}
 		 		printLists(dateListUnsorted);
 		 		}
 	   				
 			  						
-   public static void printLists(String[] list) {			//append the unsorted array to the GUI
+   public static void printLists(String[] list) {			 //append the unsorted array to the GUI
 	    	dateListSorted = new String [dateListUnsorted.length];
 	    for(int i = 0; i < dateListUnsorted.length; i++) {
-	    		 DateMap.put(list[i], null);						//append the unsorted dates to a TreeMap to be sorted 
+	    		 DateMap.put(list[i], null);			 //append the unsorted dates to a TreeMap to be sorted 
 	    	}
 	    	  DateArray = new Date212[dateListUnsorted.length];
 	    	int k =0;
